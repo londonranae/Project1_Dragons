703a12456750cd0649acd06a081ad21c295f48e9
London Conder <londonconder@gmail.com>
2022-06-07 10:54:06 -0400

    try and catch

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,46 @@ public class Main {
                 "\n is greedy and hungry and will eat you on sight." +
                 "\n Which cave will you go into? (1 or 2)");
 
-        int in = sc.nextInt();
-        {
-            if (in == 1) {
-                System.out.println("You approach the cave..." +
-                        "\n It is dark and spooky..." +
-                        "\n A large dragon jumps out in front of you! He opens his jaws and..." +
-                        "\n He gobbles you up!");
-
-            }
-                else if (in == 2) {
-                    System.out.println("You approach the cave..." +
-                            "\n It is dark and spooky..." +
-                            "\n A large dragon jumps out in front of you! He opens his jaws and..." +
-                            "\n Spits out treasure! Just for you!");
-                } else {
-                    System.out.println("Pick an actual option.");
+        boolean validation = false;
+        String input = "";
+        while (!validation) {
+            input = sc.next();
+            try {
+                Integer.parseInt(input);
+                validation = true;
+                if (validation = true) {
+                    if (Integer.parseInt(input) == 1 || Integer.parseInt(input) == 2){
+                        validation = true;
+                    } else {
+                        System.out.println("Please choose options 1 or 2.");
+                        validation = false;
+                    }
                 }
-
+            } catch (Exception e) {
+                System.out.println("Caught Exception: Invalid Choice");
+                validation = false;
             }
+
 
         }
+        if (Integer.parseInt(input) == 1) {
+            System.out.println("You approach the cave..." +
+                    "\n It is dark and spooky..." +
+                    "\n A large dragon jumps out in front of you! He opens his jaws and..." +
+                    "\n He gobbles you up!");
+
+        }
+        else if (Integer.parseInt(input) == 2) {
+            System.out.println("You approach the cave..." +
+                    "\n It is dark and spooky..." +
+                    "\n A large dragon jumps out in front of you! He opens his jaws and..." +
+                    "\n Spits out treasure! Just for you!");
+        } else {
+            System.out.println("Pick an actual option.");
+        }
+
     }
+
+}
 
 
